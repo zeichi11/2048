9f93672049c889443d7dc8eb2c413e629c1b17de
zeichi <zeichi11@gmail.com>
2019-06-06 14:34:44 +0900

    Constants.js 및 Utils.js 파일 추가
    [내용]
    - Constants.js 및 Utils.js 파일 추가
    - board 행/열 개수, action type 정의

---
+++ src/common/Constants.js
@@ -0,0 +1,13 @@
+var Constants = {
+	action: {
+		UPDATE_ROW: 'UPDATE_ROW',
+		UPDATE_COLUMN: 'UPDATE_COLUMN'
+	},
+
+	constants: {
+		TR_COUNT: 4,
+		TD_COUNT: 4
+	}
+};
+
+export default Constants;
---
+++ src/game/common/Constants.js
@@ -0,0 +1,12 @@
+var Constants = {
+	action: {
+
+	},
+
+	constants: {
+		TR_COUNT: 4,
+		TD_COUNT: 4
+	}
+};
+
+export default Constants;

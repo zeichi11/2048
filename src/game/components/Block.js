6332393fe439757f3ab405fd8bc5dfcf555424d2
zeichi <zeichi11@gmail.com>
2019-06-09 16:03:38 +0900

    Block 컴포넌트 추가
    [내용]
    - board state 값을 바탕으로 block을 랜더링하는 컴포넌트 추가

--- src/game/components/Block.js
+++ src/game/components/Block.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect, bindActionCreators } from 'react-redux';
+import Constants from '../common/Constants';
+import Utils from '../common/Utils';
 
 class Block extends Component {
 	constructor(props) {
@@ -11,8 +13,54 @@ class Block extends Component {
 	 * @returns {XML}
 	 */
 	render() {
+		const renderBlocks = (board, width, height) => {
+			const GRID_LINE_WIDTH = Constants.constants.GRID_LINE_WIDTH;
+			const TR_COUNT = Constants.constants.TR_COUNT;
+
+			const getSize = () => (width - (GRID_LINE_WIDTH * (TR_COUNT + 1))) / TR_COUNT;
+			const getPosition = (i, j, size) => {
+				let left = i * size + (GRID_LINE_WIDTH * (i + 1)),
+					top = j * size + (GRID_LINE_WIDTH * (j + 1));
+				return [top, left];
+			};
+
+			let blockList = [];
+
+			board.map((rowBlocks, i) => {
+				blockList[i] = rowBlocks.map((blockValue, j) => {
+					if (blockValue > 0) {
+						let size = getSize(i, j),
+							position = getPosition(i, j, size),
+							fontSize = size * 0.6,
+							backgroundColor = Utils.getColor(blockValue),
+							inlineStyle = {
+								'position': 'absolute',
+								'width': size + 'px',
+								'height': size + 'px',
+								'lineHeight': size + 'px',
+								'top': position[0] + 'px',
+								'left': position[1] + 'px',
+								'backgroundColor': backgroundColor,
+								'fontSize': fontSize + 'px',
+								'textAlign': 'center'
+							};
+
+						return (
+							<div key={j}
+								 style={inlineStyle}>
+								{blockValue}
+							</div>
+						);
+					}
+				});
+			});
+
+			return blockList;
+		};
+
 		return (
 			<div>
+				{renderBlocks(this.props.board, this.props.width, this.props.height)}
 			</div>
 		)
 	}

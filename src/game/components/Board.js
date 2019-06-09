abb4e284f2dc266a12bf7af9c4fbfd2eab155871
zeichi <zeichi11@gmail.com>
2019-06-09 16:04:30 +0900

    Block 컴포넌트 랜더링 로직 추가
    [내용]
    - Block 컴포넌트 랜더링 로직 추가 및 Block 생성을 위한 함수 추가

--- src/game/components/Board.js
+++ src/game/components/Board.js
@@ -2,37 +2,64 @@ import React, { Component } from 'react';
 import { connect, bindActionCreators } from 'react-redux';
 import Block from './Block';
 import Table from './Table';
+import Constants from '../common/Constants.js'
+import Utils from '../common/Utils.js'
 
 class Board extends Component {
 	constructor(props) {
 		super(props);
+		this.Initialized = false;
+	}
+
+	/**
+	 * create new block randomly
+	 */
+	createBlockRandomly() {
+		const TR_COUNT = Constants.constants.TR_COUNT;
+		const TD_COUNT = Constants.constants.TD_COUNT;
+		const BOARD_STATE = this.props.board;
+		let rowIndex = 0,
+			columnIndex = 0,
+			findEmptyIndex = false,
+			value;
+
+		while (!findEmptyIndex) {
+			rowIndex = Math.floor(Math.random() * TR_COUNT);
+			columnIndex = Math.floor(Math.random() * TD_COUNT);
+
+			if (BOARD_STATE[rowIndex][columnIndex] === 0) {
+				findEmptyIndex = true;
+			}
+		}
+
+		value = (Math.floor(Math.random() * (TR_COUNT / 2)) + 1) * 2;
+		this.props.addBlock(rowIndex, columnIndex, value);
 	}
 
 	/**
 	 *
-	 * @param {object} e
 	 */
-	handleMouseDown(e) {
-		console.log('-- mouse dpwn --');
-		console.log(e.target);
+	componentDidMount() {
+
 	}
 
-	/**
-	 *
-	 * @param {object} e
-	 */
-	handleMouseMove(e) {
-		console.log('-- mouse move --');
-		console.log(e.target);
-	}
 
-	/**
-	 * mouse up event handler
-	 * @param {object} e
-	 */
-	handleMouseUp(e) {
-		console.log('-- mouse up --');
-		console.log(e.target);
+	handleKeyDown(e) {
+		let TR_COUNT = Constants.constants.TR_COUNT,
+			TD_COUNT = Constants.constants.TD_COUNT,
+			newBoardState = Utils.cloneArray(this.props.board),
+			keyCode = e.keyCode;
+
+		switch (keyCode) {
+			case Constants.constants.KEY_CODE_LEFT:
+				break;
+			case Constants.constants.KEY_CODE_UP:
+				break;
+			case Constants.constants.KEY_CODE_RIGHT:
+				break;
+			case Constants.constants.KEY_CODE_DOWN:
+				break;
+		}
 	}
 
 	/**
@@ -40,38 +67,30 @@ class Board extends Component {
 	 * @returns {XML}
 	 */
 	render() {
-		let width,
-			height,
-			getInlineStyle = function () {
-				return {
-					width: width,
-					height: height
-				};
-			};
+		const getWidth = () => {
+			return 500;
+		};
+		const getHeight = () => {
+			return 500;
+		};
 
-		const GRID_LINE_WIDTH = 5;
-		const getWidth = function () {
-			// window 사이즈로 계산 필요
-			let width = 500;
-			return width;
-		};
-		const getHeight = function () {
-			// window 사이즈로 계싼 필요
-			let height = 500;
-			return height;
-		};
-		const renderBlocks = (board) => {
-			let blockState = board.block;
-			return blockState.map((block, i) => {
-				return (
-					<Block/>
-				);
-			})
-		};
+		let width = getWidth(),
+			height = getHeight();
 
 		return (
-			<div>
-				<Table board={this.props.board}/>
+			<div style={{'width': '100%'}}>
+				<div style={{'position': 'absolute', 'top': '50%', 'left': '50%', 'transform': 'translate(-50%, -50%)'}}>
+					<Table
+						board={this.props.board}
+						width={width}
+						height={height}
+					/>
+					<Block
+						board={this.props.board}
+						width={width}
+						height={height}
+					/>
+				</div>
 			</div>
 
 		)

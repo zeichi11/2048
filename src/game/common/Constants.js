dc20fb596eed88c6c1aaefac6008ffbeb3c31f87
zeichi <zeichi11@gmail.com>
2019-06-09 16:10:55 +0900

    constants 추가

--- src/game/common/Constants.js
+++ src/game/common/Constants.js
@@ -1,12 +1,32 @@
 var Constants = {
 	action: {
-
+		UPDATE_ROW: 'UPDATE_ROW',
+		UPDATE_COLUMN: 'UPDATE_COLUMN',
+		UPDATE_ROW_BLOCKS: 'UPDATE_ROW_BLOCKS',
+		UPDATE_COLUMN_BLOCKS: 'UPDATE_COLUMN_BLOCKS',
+		ADD_BLOCK: 'ADD_BLOCK'
 	},
 
 	constants: {
 		TR_COUNT: 4,
-		TD_COUNT: 4
-	}
+		TD_COUNT: 4,
+		GRID_LINE_WIDTH: 5,
+		KEY_CODE_LEFT: 37,
+		KEY_CODE_UP: 38,
+		KEY_CODE_RIGHT: 39,
+		KEY_CODE_DOWN: 40,
+	},
+	colors: [
+		'#47e5ae',
+		'#46e5cf',
+		'#46cde5',
+		'#46b0e5',
+		'#468de5',
+		'#467be5',
+		'#4668e5',
+		'#4668e5',
+		'#7546e5'
+	]
 };
 
 export default Constants;

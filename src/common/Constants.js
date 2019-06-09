b95dace7887fd2936ddf5b38506dbec189b2e49d
zeichi <zeichi11@gmail.com>
2019-06-09 16:05:05 +0900

    잘못된 Constants 내용 삭제
    [내용]
    - 잘못된 Constants 내용 삭제

--- src/common/Constants.js
+++ src/common/Constants.js
@@ -1,13 +1,5 @@
 var Constants = {
-	action: {
-		UPDATE_ROW: 'UPDATE_ROW',
-		UPDATE_COLUMN: 'UPDATE_COLUMN'
-	},
 
-	constants: {
-		TR_COUNT: 4,
-		TD_COUNT: 4
-	}
 };
 
 export default Constants;

6ea5a5a70bbd6319c20128854e9542fb67879eef
zeichi <zeichi11@gmail.com>
2019-06-10 01:23:44 +0900

    key 이벤트 입력에 따른 게임 컨트롤 코드 추가
    [내용]
    - 방향키 입력 시 Block의 merge, move 처리 로직 추가
    - 추후 로직 리펙토링 예정

--- src/game/components/Board.js
+++ src/game/components/Board.js
@@ -9,6 +9,9 @@ class Board extends Component {
 	constructor(props) {
 		super(props);
 		this.Initialized = false;
+		this.createBlockRandomly = this.createBlockRandomly.bind(this);
+		this.handleKeyDown = this.handleKeyDown.bind(this);
+		document.addEventListener('keydown', this.handleKeyDown);
 	}
 
 	/**
@@ -20,16 +23,22 @@ class Board extends Component {
 		const BOARD_STATE = this.props.board;
 		let rowIndex = 0,
 			columnIndex = 0,
+			count = 0,
 			findEmptyIndex = false,
 			value;
 
 		while (!findEmptyIndex) {
+			if (count > (TR_COUNT * TD_COUNT)) {
+				break;
+			}
 			rowIndex = Math.floor(Math.random() * TR_COUNT);
 			columnIndex = Math.floor(Math.random() * TD_COUNT);
 
 			if (BOARD_STATE[rowIndex][columnIndex] === 0) {
 				findEmptyIndex = true;
 			}
+
+			count++;
 		}
 
 		value = (Math.floor(Math.random() * (TR_COUNT / 2)) + 1) * 2;
@@ -43,22 +52,135 @@ class Board extends Component {
 
 	}
 
-
 	handleKeyDown(e) {
 		let TR_COUNT = Constants.constants.TR_COUNT,
 			TD_COUNT = Constants.constants.TD_COUNT,
-			newBoardState = Utils.cloneArray(this.props.board),
-			keyCode = e.keyCode;
+			newBoardState = Utils.cloneBoard(this.props.board),
+			keyCode = e.keyCode,
+			index,
+			newValue,
+			isSameState,
+			i, j;
+
+		if (!(Constants.constants.KEY_CODE_LEFT || Constants.constants.KEY_CODE_UP ||
+			Constants.constants.KEY_CODE_RIGHT || Constants.constants.KEY_CODE_DOWN)) {
+			return;
+		}
 
 		switch (keyCode) {
 			case Constants.constants.KEY_CODE_LEFT:
+				for (i = 0; i < TR_COUNT; i++) {
+					index = 0;
+					for (j = 0; j < TD_COUNT; j++) {
+						if (newBoardState[i][j] === 0) {
+							continue;
+						}
+
+						if (index > 0 && (newBoardState[i][index - 1] === newBoardState[i][j])) {
+							newValue = newBoardState[i][j] + newBoardState[i][index - 1];
+							newBoardState[i][index - 1] = newValue;
+							newBoardState[i][j] = 0;
+
+						} else {
+							newValue = newBoardState[i][j];
+							newBoardState[i][index] = newValue;
+							if (j !== index) {
+								newBoardState[i][j] = 0;
+							}
+
+							index++;
+						}
+					}
+				}
 				break;
+
 			case Constants.constants.KEY_CODE_UP:
+				for (i = 0; i < TD_COUNT; i++) {
+					index = 0;
+					for (j = 0; j < TR_COUNT; j++) {
+						if (newBoardState[j][i] === 0) {
+							continue;
+						}
+
+						if (index > 0 && (newBoardState[index - 1][i] === newBoardState[j][i])) {
+							newValue = newBoardState[j][i] + newBoardState[index - 1][i];
+							newBoardState[index - 1][i] = newValue;
+							newBoardState[j][i] = 0;
+
+						} else {
+							newValue = newBoardState[j][i];
+							newBoardState[index][i] = newValue;
+							if (j !== index) {
+								newBoardState[j][i] = 0;
+							}
+
+							index++;
+						}
+					}
+				}
 				break;
+
 			case Constants.constants.KEY_CODE_RIGHT:
+				for (i = 0; i < TR_COUNT; i++) {
+					index = TD_COUNT - 1;
+
+					for (j = TD_COUNT - 1; j >= 0; j--) {
+						if (newBoardState[i][j] === 0) {
+							continue;
+						}
+
+						if (index < TD_COUNT - 1 && (newBoardState[i][index + 1] === newBoardState[i][j])) {
+							newValue = newBoardState[i][j] + newBoardState[i][index + 1];
+							newBoardState[i][index + 1] = newValue;
+							newBoardState[i][j] = 0;
+
+						} else {
+							newValue = newBoardState[i][j];
+							newBoardState[i][index] = newValue;
+							if (j !== index) {
+								newBoardState[i][j] = 0;
+							}
+
+							index--;
+						}
+					}
+				}
 				break;
+
 			case Constants.constants.KEY_CODE_DOWN:
+				for (i = 0; i < TD_COUNT; i++) {
+					index = TR_COUNT - 1;
+
+					for (j = TR_COUNT - 1; j >= 0; j--) {
+						if (newBoardState[j][i] === 0) {
+							continue;
+						}
+
+						if (index < TR_COUNT - 1 && (newBoardState[index + 1][i] === newBoardState[j][i])) {
+							newValue = newBoardState[j][i] + newBoardState[index + 1][i];
+							newBoardState[index + 1][i] = newValue;
+							newBoardState[j][i] = 0;
+
+						} else {
+							newValue = newBoardState[j][i];
+							newBoardState[index][i] = newValue;
+							if (j !== index) {
+								newBoardState[j][i] = 0;
+							}
+
+							index--;
+						}
+					}
+				}
 				break;
+		}
+
+		isSameState = Utils.compareArrays(this.props.board, newBoardState);
+
+		// If two boards are not same then update board and create a Block randomly.
+		if (!isSameState) {
+			this.props.updateBoard(newBoardState);
+			this.createBlockRandomly();
 		}
 	}
 
